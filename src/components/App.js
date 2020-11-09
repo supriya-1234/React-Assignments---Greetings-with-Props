175a4b7741a0cb46916dafac5ac2c60e253dedbc
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-09 23:33:50 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 import Welcome from "./Welcome";
 
 const App = () => {
-  return <Welcome name="Mrigank" />;
+  return <Welcome name="Mrigank Bhaiya" />;
 };
 
 export default App;
